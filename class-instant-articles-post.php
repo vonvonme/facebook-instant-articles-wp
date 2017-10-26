41f1870c79f170481e40084c93951d2b8cc6e392
root <root@master-playbrain.c.play-brain.internal>
2017-10-26 15:50:38 +0000

    rescrape log 추가

--- class-instant-articles-post.php
+++ class-instant-articles-post.php
@@ -475,7 +475,6 @@ class Instant_Articles_Post {
         );
         $eufi_url  = get_post_meta($this->_post->ID, '_dcms_eufi_img', true);
         if ($eufi_url != null && $eufi_url != '') {
-            error_log('eufi_url = '. $eufi_url);
             $image_data['src'] = $eufi_url;
         }
         else if ( has_post_thumbnail( $this->_post->ID ) ) {
