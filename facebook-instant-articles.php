17e5a9ab0c1f469a5197db9d9029a6eedba0f0a0
lucidash <lucidash@gmail.com>
2018-06-28 21:30:14 +0900

    remove unnecessary logs

--- facebook-instant-articles.php
+++ facebook-instant-articles.php
@@ -441,7 +441,7 @@ if ( version_compare( PHP_VERSION, '5.4', '<' ) ) {
                         array(),
                         60
                     );
-                    error_log($adapter->get_canonical_url(). " rescraping res try $try:: ".$res->getBody());
+                    //error_log($adapter->get_canonical_url(). " rescraping res try $try:: ".$res->getBody());
                     if (!array_key_exists('error', json_decode($res->getBody(), true))) {
                         break;
                     }
@@ -462,7 +462,7 @@ if ( version_compare( PHP_VERSION, '5.4', '<' ) ) {
                             array(),
                             60
                         );
-                        error_log($clone_adapter->get_canonical_url(). " rescraping res try $try:: ".$res->getBody());
+                        //error_log($clone_adapter->get_canonical_url(). " rescraping res try $try:: ".$res->getBody());
                         if (!array_key_exists('error', json_decode($res->getBody(), true))) {
                             break;
                         }
